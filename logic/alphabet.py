f470bf2088e34d87a09705b909cb744c70689f53
llutsefer <n.kuziv2005@gmail.com>
2024-05-07 22:53:06 +0200

    Rename SVG files

--- logic/alphabet.py
+++ logic/alphabet.py
@@ -1,2 +1,11 @@
-class Alphabet:
+class AlphabetMeta(type):
+    _instances = {}
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            instance = super().__call__(*args, **kwargs)
+            cls._instances[cls] = instance
+        return cls._instances[cls]
+
+
+class Alphabet(metaclass=AlphabetMeta):
     pass

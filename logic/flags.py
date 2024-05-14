fabdd7830690998f0461577004abaa699f3c479f
nazar-kuziv <n.kuziv2005@gmail.com>
2024-05-14 12:29:35 +0200

    Add main functionality for all modes

--- logic/flags.py
+++ logic/flags.py
@@ -3,14 +3,81 @@ from typing import List
 
 class Flag:
     def __init__(self, letter: str, img_path: str, meaning: str, mnemonics: str, morse_code: str):
-        self.letter = letter
-        self.img_path = img_path
-        self.meaning = meaning
-        self.mnemonics = mnemonics
-        self.morse_code = morse_code
+        self._letter = letter
+        self._img_path = img_path
+        self._meaning = meaning
+        self._mnemonics = mnemonics
+        self._morse_code = morse_code
+
+    @property
+    def letter(self) -> str:
+        return self._letter
+
+    @property
+    def img_path(self) -> str:
+        return self._img_path
+
+    @property
+    def meaning(self) -> str:
+        return self._meaning
+
+    @property
+    def mnemonics(self) -> str:
+        return self._mnemonics
+
+    @property
+    def morse_code(self) -> str:
+        return self._morse_code
+
+    def check_letter(self, user_letter: str) -> bool:
+        """Checks if the letter provided by the user represents this flag
+
+        :param user_letter: Letter from user.
+        :rtype: bool
+        :return: True if the answer is correct, False otherwise.
+        """
+        user_letter = user_letter.strip().upper()
+        return user_letter == self._letter.strip().upper()
 
 
 class FlagMultiple:
     def __init__(self, flags: List[Flag], meaning: str):
-        self.flags = flags
-        self.meaning = meaning
+        self._flags = flags
+        self._meaning = meaning
+
+    @property
+    def flags(self) -> List[Flag]:
+        return self._flags
+
+    @property
+    def meaning(self) -> str:
+        return self._meaning
+
+
+class FlagSentence:
+    def __init__(self, flags: List[Flag], original_sentence: str, cleaned_sentence: str):
+        self._flags = flags
+        self._original_sentence = original_sentence
+        self._cleaned_sentence = cleaned_sentence
+
+    @property
+    def flags(self) -> List[Flag]:
+        return self._flags
+
+    @property
+    def original_sentence(self) -> str:
+        return self._original_sentence
+
+    @property
+    def cleaned_sentence(self) -> str:
+        return self._cleaned_sentence
+
+    def check_sentence(self, user_sentence: str) -> bool:
+        """Checks if the sentence provided by the user represents this FlagSentence.
+
+        :param user_sentence: Answer to check.
+        :rtype: bool
+        :return: True if the answer is correct, False otherwise.
+        """
+        user_sentence = user_sentence.strip().upper()
+        return user_sentence == self._cleaned_sentence
--- main.py
+++ main.py
@@ -2,10 +2,16 @@ import tksvg
 import tkinter as tk
 
 from logic.alphabet import Alphabet
+from logic.flags import Flag
 
-a = Alphabet()
 window = tk.Tk()
-svg_image = tksvg.SvgImage(file="graphics/" + a._characters['0'].img_path)
+a = Alphabet.get_flags_for_flashcards()
+print(a[0])
+if isinstance(a[0], Flag):
+    svg_image = tksvg.SvgImage(file="graphics/" + a[0].img_path)
+else:
+    svg_image = tksvg.SvgImage(file="graphics/" + a[0].flags[0].img_path)
+
 label = tk.Label(image=svg_image)
 label.pack()
 window.mainloop()

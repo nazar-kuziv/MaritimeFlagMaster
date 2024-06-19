31fc0171117c71baac3e17417837b2893a723860
Franzyd <firekfrank@gmail.com>
2024-06-19 22:18:16 +0200

    Finish flashcard screen

--- gui/flashcards.py
+++ gui/flashcards.py
@@ -19,8 +19,6 @@ from logic.alphabet import Alphabet
 #         self.mnemonic = mnemonic
 #         self.morse = morse
 
-testFlag = Alphabet._characters['A']
-
 # class TEMPFlagMultiple():
 #     # flags: list[str]
 #     # meaning: str
@@ -31,7 +29,7 @@ testFlag = Alphabet._characters['A']
 
 class Flashcards(ctk.CTkFrame):
     def __init__(self, master, **kwargs):
-        """Class for initializing flashcards
+        """Class for initializing the flashcard screen
 
         To draw the flashcard, call show_flashcard_front or show_flashcard_back AFTER making this frame visible with the place/pack/grid functions
         """
@@ -39,101 +37,120 @@ class Flashcards(ctk.CTkFrame):
         print("Initializing flashcards frame")
         self.master = master
 
-        # flag_list = [random.choice(list(Alphabet._characters.values()))] # randomly choose a flag, change later
-        flag_list = [Alphabet._characters['6']]
+        self.flag_list = random.sample(list(Alphabet._characters.values()), 3) # randomly choose a flag, change later
+        # self.flag_list = [Alphabet._characters['6']]
         # flag_list = [Alphabet._allFlags[0]]
 
-        self.create_flashcard(flag_list[0])
+        self.exit_button = ctk.CTkButton(self, text="Exit", width=40, command=self.exit)
+        self.exit_button.pack(side="top", anchor="nw", ipadx=10, ipady=10, padx=10, pady=10)
+
+        self.flag_index = 0
+        self.create_flashcard(self.flag_list[self.flag_index])
     
     def create_flashcard(self, flag: Flag | FlagMultiple):
         """Creates a flashcard object with the FLAG
         """
         self.flag = flag
-        print(flag)
         if (isinstance(flag, Flag)):
             self.flags = [flag]
         else:
             self.flags = flag.flags
-        # self.show_flashcard_front()
     
     def show_flashcard_base(self):
-        self.unbind("<Button-1>")
         try:
             self.flashcard.destroy()
         except AttributeError: pass
         
-        self.flashcard = ctk.CTkFrame(self, fg_color="transparent") # not the main frame but required for calculations
-        self.grid_columnconfigure(0, weight=1)
-        self.grid_rowconfigure(0, weight=1)
-        # self.flashcard = ctk.CTkFrame(self, cursor="hand2")
-        # self.flashcard.place(relx=0.5, rely=0.5, anchor="center")#, relheight=0.9, relwidth=0.9)
-        self.flashcard.grid(row=0, column=0, sticky="nsew", padx=10)
-        print(self.winfo_width())
+        self.flashcard = ctk.CTkFrame(self, cursor="hand2")
+        self.flashcard.place(relx=0.5, rely=0.5, anchor="center", relheight=0.5, relwidth=0.5)
+
+        # next button
+        try:
+            self.next_button.destroy()
+        except AttributeError: pass
+        if (self.flag_index < len(self.flag_list)-1):
+            self.next_button = ctk.CTkButton(self, text="Next", font=ctk.CTkFont(size=16), width=40, command=self.increment_flag_index)
+            self.next_button.place(relx=0.98, rely=0.5, anchor="e", relheight=0.2, relwidth=0.1)
+        
+        # back button
+        try:
+            self.back_button.destroy()
+        except AttributeError: pass
+        if (self.flag_index > 0):
+            self.back_button = ctk.CTkButton(self, text="Back", font=ctk.CTkFont(size=16), width=40, command=lambda: self.increment_flag_index(event=None, number=-1))
+            self.back_button.place(relx=0.02, rely=0.5, anchor="w", relheight=0.2, relwidth=0.1)
+
+        self.update_idletasks()
 
     def show_flashcard_front(self, event=None):
-        """Make sure to first make the main Flashcards frame visible with the place/pack/grid functions (and possibly update the main CTk window)
+        """Make sure to first make the main Flashcards frame visible with the place/pack/grid functions (and possibly update idle tasks)
         """
         self.show_flashcard_base()
         
-        self.bind("<Button-1>", self.show_flashcard_back)
         self.flashcard.bind("<Button-1>", self.show_flashcard_back)
         self.flashcard.grid_rowconfigure(0, weight=1)
         self.images = []
         for i, flag in enumerate(self.flags):
-            self.flashcard.grid_columnconfigure(i, weight=1, pad=0)
-            # img = tksvg.SvgImage(file=f"graphics/{flag.img_path}", scaletowidth=500)
-            if (self.winfo_height() < self.winfo_width() * len(self.flags)):
+            self.flashcard.grid_columnconfigure(i, weight=1)
+            if (self.flashcard.winfo_height() < self.flashcard.winfo_width() * len(self.flags)):
                 print("height smaller than width")
-                img = tksvg.SvgImage(file=f"graphics/{flag.img_path}", scaletoheight=int(self.winfo_height()*0.9/len(self.flags)))
+                img = tksvg.SvgImage(file=f"graphics/{flag.img_path}", scaletoheight=int(self.flashcard.winfo_height()*0.9/len(self.flags)))
             else:
                 print("height bigger than width")
-                img = tksvg.SvgImage(file=f"graphics/{flag.img_path}", scaletowidth=int(self.winfo_width()*0.9))
+                img = tksvg.SvgImage(file=f"graphics/{flag.img_path}", scaletowidth=int(self.flashcard.winfo_width()*0.9))
             label = ctk.CTkLabel(self.flashcard, text='', image=img)
             label.grid(row=0, column=i, sticky="nsew")
-            # label.place(relx=0.5, rely=0.5, anchor="center")
             label.bind("<Button-1>", self.show_flashcard_back)
 
             self.images.append(label)
-        print(f"Front flashcard height={self.winfo_height()} width={self.winfo_width()}")
+        print(f"Front flashcard height={self.flashcard.winfo_height()} width={self.flashcard.winfo_width()}")
     
     def show_flashcard_back(self, event=None):
-        """Make sure to first make the main Flashcards frame visible with the place/pack/grid functions (and possibly update the main CTk window)
+        """Make sure to first make the main Flashcards frame visible with the place/pack/grid functions (and possibly update idle tasks)
         """
         self.show_flashcard_base()
-        self.bind("<Button-1>", self.show_flashcard_front)
         self.flashcard.bind("<Button-1>", self.show_flashcard_front)
 
         self.flashcard.rowconfigure(0, weight=1)
         self.flashcard.rowconfigure(1, weight=3)
         self.flashcard.rowconfigure(2, weight=1)
         self.flashcard.columnconfigure(0, weight=1)
-        print(f"Back flashcard height={self.winfo_height()} width={self.winfo_width()}")
+        print(f"Back flashcard height={self.flashcard.winfo_height()} width={self.flashcard.winfo_width()}")
 
-        self.flashcard.meaning = ctk.CTkLabel(self.flashcard, text=self.flag.meaning, wraplength=int(self.master.winfo_width()*0.3), justify="left")
-        self.flashcard.meaning.grid(row=1, column=0, sticky='w')
+        self.flashcard.meaning = ctk.CTkLabel(self.flashcard, text=self.flag.meaning, wraplength=int(self.flashcard.winfo_width()*0.75), justify="left")
+        self.flashcard.meaning.grid(row=1, column=0, sticky='w', padx=10)
         self.flashcard.meaning.bind("<Button-1>", self.show_flashcard_front)
 
         isSingleFlag = isinstance(self.flag, Flag)
-        print(isSingleFlag)
         text = self.flag.letter if isSingleFlag else " ".join([x.letter for x in self.flag.flags])
         self.flashcard.letter = ctk.CTkLabel(self.flashcard, text=text)
-        self.flashcard.letter.grid(row=0, column=0, sticky='w')
+        self.flashcard.letter.grid(row=0, column=0, sticky='w', padx=10, pady=10)
         self.flashcard.letter.bind("<Button-1>", self.show_flashcard_front)
         if (not isSingleFlag): return
 
         self.flashcard.morse_code = ctk.CTkLabel(self.flashcard, text=self.flag.morse_code)
-        self.flashcard.morse_code.grid(row=2, column=0, sticky='e')
+        self.flashcard.morse_code.grid(row=2, column=0, sticky='e', padx=10, pady=10)
         self.flashcard.morse_code.bind("<Button-1>", self.show_flashcard_front)
         if (text == ""): return
 
         infoicon = tksvg.SvgImage(file="graphics/icons/info-icon.svg", scaletoheight=int(self.flashcard.letter.cget("height")))
         self.flashcard.info_mnemonic = ctk.CTkLabel(self.flashcard, text='', image=infoicon)
-        self.flashcard.info_mnemonic.grid(row=0, column=0, sticky='e')
+        self.flashcard.info_mnemonic.grid(row=0, column=0, sticky='e', padx=10, pady=10)
         CustomTooltipLabel(self.flashcard.info_mnemonic, text=self.flag.mnemonics, font=ctk.CTkFont(size=16), hover_delay=200, anchor="e")
         self.flashcard.info_mnemonic.bind("<Button-1>", self.show_flashcard_front)
 
-    # def flashcard_front_callback(self):
-    #     self.flashcard_callback("front")
+    def change_flag_index(self, event=None, index: int = 0):
+        if (index not in range(0, len(self.flag_list))):
+            raise IndexError("Index out of range")
+        self.flag_index = index
+        self.create_flashcard(self.flag_list[self.flag_index])
+        self.show_flashcard_front()
+
+    def increment_flag_index(self, event=None, number: int = 1):
+        """Adjust the flag index by the given number
+        """
+        self.change_flag_index(index=self.flag_index + number)
     
-    # def flaschard_back_callback(self):
-    #     self.flashcard_callback("back")
+    def exit(self):
+        self.master.main_menu()
+        self.destroy()
--- gui/main_window.py
+++ gui/main_window.py
@@ -9,28 +9,27 @@ class MainWindow(ctk.CTk):
 
         self.title("Maritime Flag Master")
         self.geometry("800x300")
+
+        self.main_menu()
+
+    def main_menu(self):
         self.grid_columnconfigure(0, weight=1)
         self.grid_rowconfigure(0, weight=1)
-
         self.button_frame = ctk.CTkFrame(self, fg_color="transparent")
         self.button_frame.grid(row=0, column=0, padx=10, pady=10, sticky="ns")
         self.button_frame.grid_rowconfigure(0, weight=1)
-
         buttonNames = ["Fiszki", "Słowo kodowe", "Znaczenie", "Zdanie"]
+        commands = [self.button_flashcards, None, None, None]
         self.button = [None] * 4
-        for x in range(4):
-            self.button_frame.grid_columnconfigure(x, weight=1)
-            self.button[x] = ctk.CTkButton(self.button_frame, text=buttonNames[x], command=self.button_flashcards)
-            self.button[x].grid(row=0, column=x, padx=20, pady=20, sticky="ns")
-        
-    def button_callback(self):
-        print("button pressed")
-
+        for i in range(4):
+            self.button_frame.grid_columnconfigure(i, weight=1)
+            self.button[i] = ctk.CTkButton(self.button_frame, text=buttonNames[i], command=commands[i])
+            self.button[i].grid(row=0, column=i, padx=20, pady=20, sticky="ns")
+            
     def button_flashcards(self):
         self.button_frame.destroy()
         # self.grid_columnconfigure(0, weight=1)
         # self.grid_rowconfigure(0, weight=1)
-        self.flashcards_frame = Flashcards(self, cursor="hand2")
-        self.flashcards_frame.place(relx=0.5, rely=0.5, anchor="center", relheight=0.5, relwidth=0.5)
-        self.update()
+        self.flashcards_frame = Flashcards(self, fg_color="transparent")
+        self.flashcards_frame.grid(sticky="nsew")
         self.flashcards_frame.show_flashcard_front()

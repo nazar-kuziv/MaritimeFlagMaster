973489748101e05e6f9da1ad780b30641b87bc9a
Franzyd <firekfrank@gmail.com>
2024-07-18 17:20:58 +0200

    Divide some modes into a subgroup
    Used double buffering

--- gui/main_window.py
+++ gui/main_window.py
@@ -28,13 +28,14 @@ class MainWindow(ctk.CTk):
 
     def main_menu(self):
         # self.bind("<Configure>", change_scale_size)
+        previous_widgets = self.winfo_children()
         self.update()
 
-        # self.grid_rowconfigure(0, weight=1)
-        # self.grid_rowconfigure(1, weight=5)
-        # self.grid_columnconfigure(0, weight=1)
+        self.main_frame = ctk.CTkFrame(self, fg_color="transparent")
+        self.main_frame.lower()
+        self.main_frame.place(relwidth=1, relheight=1)
 
-        self.title_frame = ctk.CTkFrame(self, fg_color='transparent')
+        self.title_frame = ctk.CTkFrame(self.main_frame, fg_color='transparent')
         self.title_frame.pack(pady=(20, 0))
         self.welcome_to = ctk.CTkLabel(self.title_frame, text='Witamy w ', 
                                        font=ctk.CTkFont(size=int(self.winfo_width()*0.017)), fg_color='transparent')
@@ -50,25 +51,28 @@ class MainWindow(ctk.CTk):
             else:
                 self.about_window.focus()
         
-        self.about = ctk.CTkFrame(self, fg_color="transparent")
+        self.about = ctk.CTkFrame(self.main_frame, fg_color="transparent")
         self.about.place(anchor="ne", rely=0, relx=1)
         self.about.button = ctk.CTkButton(self.about, text='O aplikacji', font = ctk.CTkFont(size=int(self.winfo_width()*0.013)), width=0, command=open_about_window)
         self.about.button.pack(padx=5, pady=5)
 
-        self.button_frame = ctk.CTkFrame(self, fg_color="transparent")
+        self.button_frame = ctk.CTkFrame(self.main_frame, fg_color="transparent")
         self.button_frame.pack(fill="both", expand=True, padx=10, pady=10,)
         self.button_frame.grid_rowconfigure(0, weight=1)
-        buttonNames = ["Fiszki\n\nPoznaj flagi i co oznaczają", 
-                       "Słowo kodowe\n\nDopasuj słowo kodowe MKS do flagi", 
+        buttonNames = ["Nauka\n\nPoznaj flagi i co oznaczają",
+                 "Testy\n\nSprawdź się!",
+                 "Stwórz zdjęcie\n\nZłóż własny komunikat za pomocą flag"]
+        commands = [lambda: self.new_menu(Flashcards),
+                    lambda: self.submenu(
+                        ["Słowo kodowe\n\nDopasuj słowo kodowe MKS do flagi", 
                        "Znaczenie\n\nDopasuj flagi do komunikatu", 
                        "Flagi → Zdanie\n\nPrzetłumacz zestaw flag na tekst", 
-                       "Zdanie → Flagi\n\nZakoduj komunikat za pomocą flag", 
-                       "Stwórz zdjęcie\n\nZłóż własny komunikat za pomocą flag"]
-        commands = [lambda: self.new_menu(Flashcards),
-                    lambda: self.new_menu(Codewords), 
-                    lambda: self.new_menu(Meanings), 
-                    lambda: self.new_menu(FlagSen), 
-                    lambda: self.new_menu(SenFlag),
+                       "Zdanie → Flagi\n\nZakoduj komunikat za pomocą flag"],
+                       [lambda: self.new_menu(Codewords), 
+                        lambda: self.new_menu(Meanings), 
+                        lambda: self.new_menu(FlagSen), 
+                        lambda: self.new_menu(SenFlag),]
+                    ),
                     lambda: self.new_menu(MakeImage)]
         self.button = [None] * len(buttonNames)
         for i in range(len(buttonNames)):
@@ -77,10 +81,42 @@ class MainWindow(ctk.CTk):
                                            font=ctk.CTkFont(size=int(self.button_frame.winfo_width()*0.013)), command=commands[i])
             self.button[i]._text_label.configure(wraplength=int(self.winfo_width()*0.13))
             self.button[i].grid(row=0, column=i, padx=10, pady=10, sticky="nsew")
-            
+        
+        self.update()
+        for widget in previous_widgets:
+            widget.destroy()
+
+    def submenu(self, buttonNames: list[str], commands: list):
+        # for widget in self.winfo_children():
+        #     widget.destroy()
+
+        self.sub_frame = ctk.CTkFrame(self, fg_color="transparent")
+        self.sub_frame.lower(self.main_frame)
+        self.sub_frame.place(relwidth=1, relheight=1)
+
+        self.exit_button = ctk.CTkButton(self.sub_frame, text="Wróć", width=0, font=ctk.CTkFont(size=int(self.winfo_width()*0.015)), fg_color="orange red", command=self.main_menu)
+        self.exit_button.pack(side="top", anchor="nw", ipadx=10, ipady=10, padx=10, pady=(10, 0))
+
+        self.button_frame = ctk.CTkFrame(self.sub_frame, fg_color="transparent")
+        self.button_frame.pack(fill="both", expand=True, padx=10, pady=10,)
+        self.button_frame.grid_rowconfigure(0, weight=1)
+        self.button = [None] * len(buttonNames)
+        for i in range(len(buttonNames)):
+            self.button_frame.grid_columnconfigure(i, weight=1, uniform="yes")
+            self.button[i] = ctk.CTkButton(self.button_frame, text=buttonNames[i], 
+                                           font=ctk.CTkFont(size=int(self.button_frame.winfo_width()*0.013)), command=commands[i])
+            self.button[i]._text_label.configure(wraplength=int(self.winfo_width()*0.13))
+            self.button[i].grid(row=0, column=i, padx=10, pady=10, sticky="nsew")
+        
+        self.update()
+        self.main_frame.destroy()
+
     def new_menu(self, menu_callback):
-        self.title_frame.destroy()
-        self.button_frame.destroy()
+        previous_widgets = self.winfo_children()
         self.new_frame = menu_callback(self, fg_color="transparent")
+        self.new_frame.lower()
         self.new_frame.pack(fill="both", expand=True)
         self.new_frame.start()
+        self.update()
+        for widget in previous_widgets:
+            widget.destroy()

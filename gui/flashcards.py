f80562b9fd6aaa9e0a041096eddec1b2c76ab006
Franzyd <firekfrank@gmail.com>
2024-12-27 19:21:12 +0100

    Rearrange flashcard layout

--- gui/flashcards.py
+++ gui/flashcards.py
@@ -121,32 +121,32 @@ class Flashcards(Util.AppQuizPage):
         self.flashcard.columnconfigure(0, weight=1)
         # print(f"Back flashcard height={self.flashcard.winfo_height()} width={self.flashcard.winfo_width()}")
 
-        self.flashcard.meaning = ctk.CTkLabel(self.flashcard, text=self.flag.meaning, font=ctk.CTkFont(size=int(self.master.scale_size*0.04)), wraplength=int(self.flashcard.winfo_width()*0.75), justify="left")
-        self.flashcard.meaning.grid(row=1, column=0, sticky='w', padx=10)
+        self.flashcard.meaning = ctk.CTkLabel(self.flashcard, text=self.flag.meaning, font=ctk.CTkFont(size=int(self.master.scale_size*0.035)), wraplength=int(self.flashcard.winfo_width()*0.75))
+        self.flashcard.meaning.grid(row=1, column=0, padx=10)
         self.flashcard.meaning.bind("<Button-1>", self.show_flashcard_front)
 
         isSingleFlag = isinstance(self.flag, Flag)
         if (isSingleFlag):
-            if (any(i in self.flag.code_word for i in
-                    ''.join(x for x in Alphabet._additionalFlags.keys())
-                    )): text = ""
-            else: text = self.flag.code_word
+            if (self.flag.morse_code is ""): text = ""
+            else: text = "Kod: " + self.flag.code_word
         else:
-            text = " ".join([x.code_word for x in self.flag.flags])
-        self.flashcard.letter = ctk.CTkLabel(self.flashcard, text=text, font=ctk.CTkFont(size=int(self.master.scale_size*0.035)))
-        self.flashcard.letter.grid(row=0, column=0, sticky='w', padx=10, pady=10)
-        self.flashcard.letter.bind("<Button-1>", self.show_flashcard_front)
+            text = "Kod: " + " ".join([x.code_word if x.morse_code is not "" else x.meaning for x in self.flag.flags])
+        self.flashcard.code_word = ctk.CTkLabel(self.flashcard, text=text, font=ctk.CTkFont(size=int(self.master.scale_size*0.03)))
+        self.flashcard.code_word.grid(row=0, column=0, sticky='w', padx=10, pady=10)
+        self.flashcard.code_word.bind("<Button-1>", self.show_flashcard_front)
         if (not isSingleFlag): return
 
         self.flashcard.morse = ctk.CTkFrame(self.flashcard, fg_color="transparent")
         self.flashcard.morse.grid(column=0, row=2, sticky="w")
+        morse_text = self.flag.morse_code
+        if (morse_text != ""): morse_text = "Morse:  " + morse_text
 
-        self.flashcard.morse.morse_code = ctk.CTkLabel(self.flashcard.morse, text=self.flag.morse_code, font=ctk.CTkFont(size=int(self.master.scale_size*0.035)))
+        self.flashcard.morse.morse_code = ctk.CTkLabel(self.flashcard.morse, text=morse_text, font=ctk.CTkFont(size=int(self.master.scale_size*0.03)))
         self.flashcard.morse.morse_code.pack(side="left", padx=10, pady=10)
         self.flashcard.morse.morse_code.bind("<Button-1>", self.show_flashcard_front)
         if (text == ""): return
 
-        infoicon = tksvg.SvgImage(file=Environment.resource_path("static/graphics/icons/info-icon.svg"), scaletoheight=int(self.master.scale_size*0.05))
+        infoicon = tksvg.SvgImage(file=Environment.resource_path("static/graphics/icons/lightbulb-icon.svg"), scaletoheight=int(self.master.scale_size*0.04))
         self.flashcard.flag_mnemonic = ctk.CTkLabel(self.flashcard, text='', image=infoicon)
         self.flashcard.flag_mnemonic.grid(row=0, column=0, sticky='ne', padx=10, pady=10)
         CustomTooltipLabel(self.flashcard.flag_mnemonic, text=f"Skojarzenie mnemotechniczne:\n{self.flag.flag_mnemonics}", font=ctk.CTkFont(size=20), hover_delay=200, anchor="e")

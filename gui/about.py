c26435ea2e6b49d64d05e4d4e77855e0da71dc2a
Franzyd <firekfrank@gmail.com>
2025-06-26 13:43:03 +0200

    Add sources

--- gui/about.py
+++ gui/about.py
@@ -1,8 +1,8 @@
 import customtkinter as ctk
 import webbrowser
 
-githubKuziv = 'https://github.com/nazar-kuziv'
-githubFranzyd = 'https://github.com/Franzyd'
+githubKuziv = 'github.com/nazar-kuziv'
+githubFranzyd = 'github.com/Franzyd'
 
 class AboutWindow(ctk.CTkToplevel):
     def __init__(self, *args, **kwargs):
@@ -14,25 +14,54 @@ class AboutWindow(ctk.CTkToplevel):
         self.authors = ctk.CTkLabel(self, text='Autorzy:', fg_color='transparent', font=ctk.CTkFont(weight='bold'))
         self.authors.pack()
 
-        self.textbox = ctk.CTkTextbox(self, width=300, height=400, activate_scrollbars=False, wrap="word", fg_color='transparent')
-        self.textbox.pack()
+        self.description = ctk.CTkTextbox(self, width=370, height=280, activate_scrollbars=False, wrap="word", fg_color='transparent')
+        self.description.pack()
         
-        self.textbox.tag_config('name', foreground="green4")
-        self.textbox.tag_config('link', foreground="blue")
-        self.textbox.tag_bind('link', '<Enter>', lambda x: self.textbox.configure(cursor='hand2'))
-        self.textbox.tag_bind('link', '<Leave>', lambda x: self.textbox.configure(cursor=''))
-        self.textbox.tag_bind('gitKuziv', '<1>', lambda x: webbrowser.open_new(githubKuziv))
-        self.textbox.tag_bind('gitFranzyd', '<1>', lambda x: webbrowser.open_new(githubFranzyd))
+        self.description.tag_config('name', foreground="green4")
+        self.description.tag_config('link', foreground="blue")
+        self.description.tag_bind('link', '<Enter>', lambda x: self.description.configure(cursor='hand2'))
+        self.description.tag_bind('link', '<Leave>', lambda x: self.description.configure(cursor=''))
+        self.description.tag_bind('gitKuziv', '<1>', lambda x: webbrowser.open_new(f'https://{githubKuziv}'))
+        self.description.tag_bind('gitFranzyd', '<1>', lambda x: webbrowser.open_new(f'https://{githubFranzyd}'))
 
-        self.textbox.insert('end', 'Nazar Kuziv', 'name')
-        self.textbox.insert('end', '\nE-mail:  n.kuziv2005@gmail.com\nGitHub: ')
-        self.textbox.insert('end', githubKuziv, ('link', 'gitKuziv'))
-        self.textbox.insert('end', '\n\nFranciszek Firek', 'name')
-        self.textbox.insert('end', '\nE-mail: firekfrank@gmail.com\nGitHub: ')
-        self.textbox.insert('end', githubFranzyd, ('link', 'gitFranzyd'))
+        self.description.insert('end', 'Nazar Kuziv', 'name')
+        self.description.insert('end', '\nE-mail:  n.kuziv2005@gmail.com\nGitHub: ')
+        self.description.insert('end', githubKuziv, ('link', 'gitKuziv'))
+        self.description.insert('end', '\n\nFranciszek Firek', 'name')
+        self.description.insert('end', '\nE-mail: firekfrank@gmail.com\nGitHub: ')
+        self.description.insert('end', githubFranzyd, ('link', 'gitFranzyd'))
 
-        self.textbox.insert('end',
+        self.description.insert('end',
                             '\n\nAplikacja MaritimeFlagMaster została stworzona przez pasjonatów programowania i edukacji, Nazara Kuziva i Franciszka Firka. Naszym celem jest umożliwienie użytkownikom efektywnego i przyjemnego przyswajania wiedzy na temat międzynarodowych flag sygnałowych. Jesteśmy otwarci na wszelkie sugestie i uwagi, które mogą pomóc w udoskonaleniu naszej aplikacji. Zachęcamy do kontaktu poprzez e-mail.')
 
-        self.textbox.configure(state='disabled')
+        self.description.configure(state='disabled')
+
+        links = ['www.scribd.com/document/689336702/MKS', 'www.scribd.com/user/708834842/tomczak-s2000',
+                 'en.wikipedia.org/wiki/International_Code_of_Signals', r'pl.wikipedia.org/wiki/Kod_Morse%E2%80%99a']
+
+        self.sources = ctk.CTkLabel(self, text='Źródła:', fg_color='transparent', font=ctk.CTkFont(weight='bold'))
+        self.sources.pack()
+
+        self.links = ctk.CTkTextbox(self, width=370, height=200, activate_scrollbars=False, wrap="word", fg_color='transparent')
+        self.links.pack()
+
+        self.links.tag_config('link', foreground="blue")
+        self.links.tag_bind('link', '<Enter>', lambda x: self.links.configure(cursor='hand2'))
+        self.links.tag_bind('link', '<Leave>', lambda x: self.links.configure(cursor=''))
+        self.links.tag_bind('link0', '<1>', lambda x: webbrowser.open_new(f'https://{links[0]}'))
+        for i, l in enumerate(links):
+            self.links.tag_bind(f'link{i}', '<1>', lambda x, url=l: webbrowser.open_new(f'https://{url}'))
+        
+        self.links.insert('end', 'Większość skojarzeń mnemotechnicznych do flag i wszystkie znaczenia do jednoznakowych flag:\n')
+        self.links.insert('end', links[0], ('link', 'link0'))
+        self.links.insert('end', '\nWgrane przez ')
+        self.links.insert('end', 'tomczak.s2000', ('link', 'link1'))
+        self.links.insert('end', '\n\nZnaczenia do sygnałów wieloznakowych (przetłumaczone z angielskiego):\n')
+        self.links.insert('end', links[2], ('link', 'link2'))
+        self.links.insert('end', '\n\nSkojarzenia mnemotechniczne do kodu Morse\'a:\n')
+        self.links.insert('end', links[3], ('link', 'link3'))
+
+        self.links.configure(state='disabled')
+
         self.resizable(False, False)
+        self.after(1, self.focus)
